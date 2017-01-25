535aafc4458e3846bedc6c4b82498b2bc0716d9a
zippyl <zippy@ex.ua>
2017-01-25 23:55:10 +0200

    add settings panel, shop panel fix some bugs fix some menu items

--- Assets/Scripts/Menu/UI/UIMenuController.cs
+++ Assets/Scripts/Menu/UI/UIMenuController.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMenuController : MonoBehaviour {
+
+    [SerializeField] private GameObject _settingsPanel, _settingsPanelChild,
+        _shopPanel;
 
     // Use this for initialization
     void Start() {
@@ -14,9 +18,10 @@ public class UIMenuController : MonoBehaviour {
 
     }
 
+    #region Open
     public void OpenShop()
     {
-
+        _shopPanel.SetActive(true);
     }
 
     public void OpenAchivements()
@@ -31,6 +36,52 @@ public class UIMenuController : MonoBehaviour {
 
     public void OpenSettings()
     {
+        _settingsPanel.SetActive(true);
+        AnimationObjectOn(_settingsPanel, _settingsPanelChild);
+    }
+    #endregion
+
+    #region Close
+    public void CloseShop()
+    {
+        _shopPanel.SetActive(false);
+    }
+
+    public void CloseAchivements()
+    {
 
     }
+
+    public void CloseLeaderboard()
+    {
+
+    }
+
+    public void CloseSettings()
+    {
+        AnimationObjectOut(_settingsPanel, _settingsPanelChild);
+    }
+    #endregion
+
+    #region Animation
+    void AnimationObjectOn(GameObject mainGO, GameObject panelGO)
+    {
+        mainGO.GetComponent<CanvasRenderer>().SetAlpha(0.01f);
+        mainGO.GetComponent<Image>().CrossFadeAlpha(1f, 0.5f, true);
+        panelGO.transform.position = new Vector2(0, mainGO.transform.position.y - 10f);
+        LeanTween.move(panelGO, new Vector2(0, mainGO.transform.position.y), .5f).setEase(LeanTweenType.easeInOutSine).setIgnoreTimeScale(true);
+    }
+    void AnimationObjectOut(GameObject mainGO, GameObject panelGO)
+    {
+        mainGO.GetComponent<Image>().CrossFadeAlpha(0f, 0.5f, true);
+        LeanTween.move(panelGO, new Vector2(0, mainGO.transform.position.y + 10f), .5f).setEase(LeanTweenType.easeInOutSine).setIgnoreTimeScale(true);
+        StartCoroutine(WaitASec(mainGO));
+    }
+
+    IEnumerator WaitASec(GameObject go)
+    {
+        yield return new WaitForSeconds(.5f);
+        go.SetActive(false);
+    }
+    #endregion Animations
 }
