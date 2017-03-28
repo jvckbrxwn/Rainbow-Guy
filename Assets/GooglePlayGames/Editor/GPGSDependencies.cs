c03f62434f76f6cf1c4dcbf128853ec6e3d047e4
zippyl <zippy@ex.ua>
2017-03-29 00:38:41 +0300

    google play plugin fix

--- Assets/GooglePlayGames/Editor/GPGSDependencies.cs
+++ Assets/GooglePlayGames/Editor/GPGSDependencies.cs
@@ -17,14 +17,14 @@
 namespace GooglePlayGames.Editor
 {
 
-using System;
-using System.Collections.Generic;
-using UnityEditor;
+    using System;
+    using System.Collections.Generic;
+    using UnityEditor;
 
-/// AdMob dependencies file.
-[InitializeOnLoad]
-public class GPGSDependencies : AssetPostprocessor
-{
+    /// AdMob dependencies file.
+    [InitializeOnLoad]
+    public class GPGSDependencies : AssetPostprocessor
+    {
 #if UNITY_ANDROID
         /// <summary>Instance of the PlayServicesSupport resolver</summary>
         public static object svcSupport;
@@ -33,13 +33,15 @@ public class GPGSDependencies : AssetPostprocessor
         /// Initializes static members of the class.
         static GPGSDependencies() { RegisterDependencies(); }
 
-        public static void RegisterDependencies() {
+        public static void RegisterDependencies()
+        {
 #if UNITY_ANDROID
             // Setup the resolver using reflection as the module may not be
             // available at compile time.
             Type playServicesSupport = Google.VersionHandler.FindClass(
                 "Google.JarResolver", "Google.JarResolver.PlayServicesSupport");
-            if (playServicesSupport == null) {
+            if (playServicesSupport == null)
+            {
                 return;
             }
             svcSupport = svcSupport ?? Google.VersionHandler.InvokeStaticMethod(
@@ -92,47 +94,25 @@ public class GPGSDependencies : AssetPostprocessor
                 new object[] { "com.android.support", "support-v4", "23.1+" },
                 namedArgs: new Dictionary<string, object>() {
                     {"packageIds", new string[] { "extra-android-m2repository" } }
-                });
-#elif UNITY_IOS && !NO_GPGS
-            /*
-            *
-            *  THIS IS A TEMPORARY HACK!
-            *
-            *   The ios resolver is not handling static libraries correctly,
-            *   as a result, the project is left in a bad state.
-            *
-            *   This disables the resolver for iOS, and developers can manually
-            *   create a podfile and use cocoapods to set up the xcode project.
-            *
-            */
-            Google.IOSResolver.Enabled = false;
-            Type iosResolver = Google.VersionHandler.FindClass(
-                "Google.IOSResolver", "Google.IOSResolver");
-            if (iosResolver == null) {
-                return;
-            }
-            Google.VersionHandler.InvokeStaticMethod(
-                iosResolver, "AddPod",
-                new object[] { "GooglePlayGames" },
-                namedArgs: new Dictionary<string, object>() {
-                    { "version", "5.1.2" },
-                    { "bitcodeEnabled", false },
-                });
+                });            
 #endif  // UNITY_IOS
         }
 
         // Handle delayed loading of the dependency resolvers.
         private static void OnPostprocessAllAssets(
                 string[] importedAssets, string[] deletedAssets,
-                string[] movedAssets, string[] movedFromPath) {
-            foreach (string asset in importedAssets) {
+                string[] movedAssets, string[] movedFromPath)
+        {
+            foreach (string asset in importedAssets)
+            {
                 if (asset.Contains("IOSResolver") ||
-                    asset.Contains("JarResolver")) {
+                    asset.Contains("JarResolver"))
+                {
                     RegisterDependencies();
                     break;
                 }
             }
         }
-}
+    }
 
 }
