b4ff72f3561c00db977a5c10d16de0633c9314f4
Kokoshko Stanislav <ctacoh15l@gmail.com>
2023-11-13 21:19:49 +0200

    remove debug

--- Assets/Scripts/Managers/Base/BaseManager.cs
+++ Assets/Scripts/Managers/Base/BaseManager.cs
@@ -1,13 +1,10 @@
 using Managers.Interfaces;
-using UnityEngine;
 
 namespace Managers.Base
 {
 	public class BaseManager : IInitializable
 	{
 		protected BaseManager()
-		{
-			Debug.Log($"Hello, I'm {this.GetType()}");
-		}
+		{ }
 	}
 }
